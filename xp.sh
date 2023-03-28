45880704318535df194145e499f9303fa2a03f40
keyjsl <111936411+keyjsl@users.noreply.github.com>
2023-03-28 18:29:47 +0800

    Update xp.sh

--- xp.sh
+++ xp.sh
@@ -3,61 +3,6 @@
 GitUser="keyjsl"
 #IZIN SCRIPT
 MYIP=$(curl -sS ipv4.icanhazip.com)
-# SHADOWSOCK
-data=( `cat /etc/shadowsocks-libev/akun.conf | grep '^###' | cut -d ' ' -f 2`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-exp=$(grep -w "^### $user" "/etc/shadowsocks-libev/akun.conf" | cut -d ' ' -f 3)
-d1=$(date -d "$exp" +%s)
-d2=$(date -d "$now" +%s)
-exp2=$(( (d1 - d2) / 86400 ))
-if [[ "$exp2" = "0" ]]; then
-sed -i "/^### $user $exp/,/^port_http/d" "/etc/shadowsocks-libev/akun.conf"
-systemctl disable shadowsocks-libev-server@$user-tls.service
-systemctl disable shadowsocks-libev-server@$user-http.service
-systemctl stop shadowsocks-libev-server@$user-tls.service
-systemctl stop shadowsocks-libev-server@$user-http.service
-rm -f "/etc/shadowsocks-libev/$user-tls.json"
-rm -f "/etc/shadowsocks-libev/$user-http.json"
-rm -f "/home/vps/public_html/$user-yaml-tls.yaml"
-rm -f "/home/vps/public_html/$user-yaml-http.yaml"
-fi
-done
-# SHADOWSOCKR
-data=( `cat /usr/local/shadowsocksr/akun.conf | grep '^###' | cut -d ' ' -f 2`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-exp=$(grep -w "^### $user" "/usr/local/shadowsocksr/akun.conf" | cut -d ' ' -f 3)
-d1=$(date -d "$exp" +%s)
-d2=$(date -d "$now" +%s)
-exp2=$(( (d1 - d2) / 86400 ))
-if [[ "$exp2" = "0" ]]; then
-sed -i "/^### $user $exp/d" "/usr/local/shadowsocksr/akun.conf"
-rm -f "/usr/local/shadowsocksr/$user-clash-for-android.yaml"
-rm -f "/home/vps/public_html/$user-clash-for-android.yaml"
-cd /usr/local/shadowsocksr
-match_del=$(python mujson_mgr.py -d -u "${user}"|grep -w "delete user")
-cd
-fi
-done
-/etc/init.d/ssrmu restart
-# WIREGUARD
-data=( `cat /etc/wireguard/wg0.conf | grep '^### Client' | cut -d ' ' -f 3`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-exp=$(grep -w "^### Client $user" "/etc/wireguard/wg0.conf" | cut -d ' ' -f 4)
-d1=$(date -d "$exp" +%s)
-d2=$(date -d "$now" +%s)
-exp2=$(( (d1 - d2) / 86400 ))
-if [[ "$exp2" = "0" ]]; then
-sed -i "/^### Client $user $exp/,/^AllowedIPs/d" /etc/wireguard/wg0.conf
-rm -f "/home/vps/public_html/$user.conf"
-fi
-done
-systemctl restart wg-quick@wg0
 # XRAY VMESS WS
 data=( `cat /usr/local/etc/xray/config.json | grep '^###' | cut -d ' ' -f 2`);
 now=`date +"%Y-%m-%d"`
@@ -126,18 +71,3 @@ sed -i "/^### $user $exp/,/^},{/d" /usr/local/etc/xray/xtls.json
 fi
 done
 systemctl restart xray@xtls
-# TROJAN GO
-data=( `cat /etc/trojan-go/akun.conf | grep '^###' | cut -d ' ' -f 2`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-exp=$(grep -w "^### $user" "/etc/trojan-go/akun.conf" | cut -d ' ' -f 3)
-d1=$(date -d "$exp" +%s)
-d2=$(date -d "$now" +%s)
-exp2=$(( (d1 - d2) / 86400 ))
-if [[ "$exp2" = "0" ]]; then
-sed -i '/^,"'"$user"'"$/d' /etc/trojan-go/config.json
-sed -i "/^### $user $exp/d" "/etc/trojan-go/akun.conf"
-fi
-done
-systemctl restart trojan-go
