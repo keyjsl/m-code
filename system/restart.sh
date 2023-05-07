ad1c09a2579e280b50e5ed94285fb4e43a8a39d1
keyjsl <111936411+keyjsl@users.noreply.github.com>
2023-05-07 16:55:05 +0800

    Update restart.sh

--- system/restart.sh
+++ system/restart.sh
@@ -9,14 +9,20 @@ echo -e "======================================"
 echo -e "         \e[0;32mRESTART VPN SERVICE\e[0m"
 echo -e "======================================"
 echo -e "  \e[0;32m[•1]\e[0m Restart All Services"
-echo -e "  \e[0;32m[•2]\e[0m Restart Nginx"
-echo -e "  \e[0;32m[•3]\e[0m Restart Vmess & Vless"
-echo -e "  \e[0;32m[•4]\e[0m Restart Trojan"
-echo -e "  \e[0;32m[•4]\e[0m Restart ShadowsocksR"
+echo -e "  \e[0;32m[•2]\e[0m Restart OpenSSH"
+echo -e "  \e[0;32m[•3]\e[0m Restart Dropbear"
+echo -e "  \e[0;32m[•4]\e[0m Restart Squid"
+echo -e "  \e[0;32m[•5]\e[0m Restart Nginx"
+echo -e "  \e[0;32m[•6]\e[0m Restart Vmess & Vless"
+echo -e "  \e[0;32m[•7]\e[0m Restart Trojan"
+echo -e "  \e[0;32m[•8]\e[0m Restart ShadowsocksR"
+echo -e "  \e[0;32m[•9]\e[0m Restart Badvpn"
+echo -e "  \e[0;32m[10]\e[0m Restart OHP Port"
+echo -e "  \e[0;32m[11]\e[0m Restart WebSocket Port"
 echo -e "======================================"
 echo -e "   \e[0;32m[x]\e[0m     \e[1;31mMain Menu\e[0m"
 echo -e ""
-read -p "    Select From Options [1-5 or x] :" Restart
+read -p "    Select From Options [1-11 or x] :" Restart
 echo -e ""
 echo -e "======================================"
 sleep 1
@@ -24,8 +30,12 @@ clear
 case $Restart in
                 1)
                 clear
+                /etc/init.d/ssh restart
+                /etc/init.d/dropbear restart
+                /etc/init.d/fail2ban restart
                 /etc/init.d/cron restart
                 /etc/init.d/nginx restart
+                /etc/init.d/squid restart
 				systemctl restart xray
 				systemctl restart xray@none
 				systemctl restart xray@vless
@@ -39,6 +49,15 @@ case $Restart in
 				systemctl restart xray@trojan
 				systemctl restart trojan-go
 				/etc/init.d/ssrmu restart
+				systemctl restart ohp
+				systemctl restart ohpd
+				systemctl restart ohps
+			    systemctl restart cdn-dropbear
+				systemctl restart cdn-ovpn
+				systemctl restart cdn-ssl
+                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 1000
+                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 1000
+                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000
                 echo -e ""
                 echo -e "======================================"
                 echo -e ""
@@ -47,7 +66,48 @@ case $Restart in
                 echo -e "======================================"
                 exit
                 ;;
+               echo -e ""
+                echo -e "======================================"
+                echo -e ""
+                echo -e "          \e[0;32mALL Service Restarted\e[0m         "
+                echo -e ""
+                echo -e "======================================"
+                exit
+                ;;
                 2)
+                clear
+                /etc/init.d/ssh restart
+                echo -e ""
+                echo -e "======================================"
+                echo -e ""
+                echo -e "        \e[0;32mSSH Service Restarted\e[0m       "
+                echo -e ""
+                echo -e "======================================"
+                exit
+                ;;
+                3)
+                clear
+                /etc/init.d/dropbear restart
+                echo -e ""
+                echo -e "======================================"
+                echo -e ""
+                echo -e "       \e[0;32mDropbear Service Restarted\e[0m     "
+                echo -e ""
+                echo -e "======================================"
+                exit
+                ;;
+                4)
+                clear
+                /etc/init.d/squid restart
+                echo -e ""
+                echo -e "======================================"
+                echo -e ""
+                echo -e "        \e[0;32mSquid3 Service Restarted\e[0m      "
+                echo -e ""
+                echo -e "======================================"
+                exit
+                ;;
+                5)
                 clear
                 /etc/init.d/nginx restart
                 echo -e ""
@@ -58,7 +118,7 @@ case $Restart in
                 echo -e "======================================"
                 exit
                 ;;
-				3)
+				6)
                 clear
 				systemctl restart xray
 				systemctl restart xray@none
@@ -75,7 +135,7 @@ case $Restart in
                 echo -e "======================================"
                 exit
                 ;;
-				4)
+				7)
 				clear
 				systemctl restart trojan-go
                 echo -e ""
@@ -86,13 +146,58 @@ case $Restart in
                 echo -e "======================================"
                 exit
                 ;;
-				5)
+				8)
 				clear
                 /etc/init.d/ssrmu restart
                 echo -e ""
                 echo -e "======================================"
                 echo -e ""
                 echo -e "      \e[0;32mShadowsockR Service Restart\e[0m     "
+                echo -e ""
+                echo -e "======================================"
+                exit
+                ;;
+		9)
+                clear
+                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500
+                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500
+                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
+                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7400 --max-clients 500
+                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7500 --max-clients 500
+                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7600 --max-clients 500
+                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7700 --max-clients 500
+                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7800 --max-clients 500
+                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7900 --max-clients 500
+                echo -e ""
+                echo -e "======================================"
+                echo -e ""
+                echo -e "       \e[0;32mBadvpn Service Restarted\e[0m     "
+                echo -e ""
+                echo -e "======================================"
+                exit
+                ;;
+				10)
+				clear
+                systemctl restart ohp
+				systemctl restart ohpd
+				systemctl restart ohps
+                echo -e ""
+                echo -e "======================================"
+                echo -e ""
+                echo -e "         \e[0;32mOHP Service Restarted\e[0m     "
+                echo -e ""
+                echo -e "======================================"
+                exit
+				;;
+				11)
+				clear
+                systemctl restart cdn-dropbear
+				systemctl restart cdn-ovpn
+				systemctl restart cdn-ssl
+                echo -e ""
+                echo -e "======================================"
+                echo -e ""
+                echo -e "      \e[0;32mWebSocket Service Restarted\e[0m     "
                 echo -e ""
                 echo -e "======================================"
                 exit
